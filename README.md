b819b79f6a99010e275434134fa7948225319f46
Sandoche Adittane <sandoche@protonmail.com>
2020-07-26 20:54:24 +0200

    :tada: Fork from detoxify edited for a proof of concept

---
+++ README.md
@@ -0,0 +1,74 @@
+<p align="center">
+  <img src="/src/public/images/icon.png" width="150">
+</p>
+<h1 align="center">Detoxify</h1>
+<p>
+  <img src="https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000" />
+  <a href="/LICENSE">
+    <img alt="License: Apache 2.0" src="https://img.shields.io/badge/License-Apache 2.0-yellow.svg" target="_blank" />
+  </a>
+
+  <a href="https://t.me/coin_generator">
+    <img alt="Telegram" src="https://img.shields.io/badge/chat-Telegram-blue.svg" target="_blank" />
+  </a>
+
+  <a href="https://twitter.com/farbodsaraf">
+    <img alt="Twitter: farbodsaraf" src="https://img.shields.io/twitter/follow/farbodsaraf.svg?style=social" target="_blank" />
+  </a>
+</p>
+
+Generate a fake app to replace any addictive app 📱🙅
+
+Detoxify.app is a website to generate a fake of any app to replace it and help you in your digital detox process.
+It is built with `Express.js` and uses `google-play-scraper` package to search in Google Play Store and to get the metadata.
+
+### 👉 [Get started](https://detoxify.app)
+<br>
+
+## 📖 How to use
+
+### Install
+
+```sh
+npm install
+```
+
+### Usage
+
+```sh
+npm run start
+```
+
+### Development
+
+```sh
+npm run dev
+```
+
+### Deploy to Heroku
+Since this project scraps data from Google Play, the number of queries are limited per server, therefore in case of problem we recommend you to host your own instance of Detofixy.<br><br>
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Authors
+
+👤 **Sandoche ADITTANE & Farbod SARAF**
+
+* Twitter: [@farbodsaraf](https://twitter.com/farbodsaraf)
+* Github: [@sandoche](https://github.com/sandoche)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+## ⭐️ Show your support
+
+Give a ⭐️ if this project helped you!
+
+## ☕️ Buy me a coffee 
+
+If you like this project, feel free to donate: https://www.buymeacoffee.com/detoxify
+
+## 📝 License
+
+Copyright © 2019 [Sandoche ADITTANE](https://www.sandoche.com) & [Farbod SARAF](https://farbodsaraf.com/)<br />
+This project is [Apache 2.0](/LICENSE) licensed.
---
+++ src/config.js
@@ -0,0 +1,62 @@
+/*
+  Copyright 2019 Sandoche ADITTANE & Farbod SARAF
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+const config = {
+  title: 'Detoxify.app',
+  short_description: 'Digital detox made easy',
+  description: 'Generate a fake app to replace any addictive app 📱🙅',
+  long_description: 'Use AppDetox to help you detox from any addictive app',
+  keywords: 'digital detox, detox, pwa',
+  share: 'Use this app to detox from ',
+  og_title: ' Fake detox app',
+  og_description: 'An fake app to help you detox from ',
+  popular: 'Most popular',
+  google_analytics: 'UA-143582756-1',
+  suggestions: [
+    {
+      name: 'Facebook',
+      appId: 'com.facebook.katana',
+      image: 'https://lh3.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg'
+    },
+    {
+      name: 'Instagram',
+      appId: 'com.instagram.android',
+      image: 'https://lh3.googleusercontent.com/2sREY-8UpjmaLDCTztldQf6u2RGUtuyf6VT5iyX3z53JS4TdvfQlX-rNChXKgpBYMw'
+    },
+    {
+      name: 'Snapchat',
+      appId: 'com.snapchat.android',
+      image: 'https://lh3.googleusercontent.com/ujz8cInt2YuaGLo2wahih771yXzWbyhNhzrCzm5Y5omDnLRt3v-FTTNMVfgT7FgIEfI'
+    },
+    {
+      name: 'YouTube',
+      appId: 'com.google.android.youtube',
+      image: 'https://lh3.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc'
+    },
+    {
+      name: 'Twitter',
+      appId: 'com.twitter.android',
+      image: 'https://lh3.googleusercontent.com/wIf3HtczQDjHzHuu7vezhqNs0zXAG85F7VmP7nhsTxO3OHegrVXlqIh_DWBYi86FTIGk'
+    },
+    {
+      name: '9gag',
+      appId: 'com.ninegag.android.app',
+      image: 'https://lh3.googleusercontent.com/QiE-x76mMbIxT5W2sBCdORRnz-YJys2ridssRJZmiE540_5ADuFnOg-9I0H6bTQOuw'
+    }        
+  ]
+}
+
+export default config;
