acc80cdc1240d5c938d34def743c82bd42b72c99
Farbod Saraf <farbodsaraf@users.noreply.github.com>
2020-11-01 00:24:00 +0100

    Add title and logos

--- src/data/websites.js
+++ src/data/websites.js
@@ -16,49 +16,57 @@
 
 const websites = [
   {
+    title: 'Twitter',
     appId: 'com.twitter.android',
+    icon: 'https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/1259px-Twitter_bird_logo_2012.svg.png',
     developerWebsite: 'https://twitter.com/'
   },
   {
+    title: 'Facebook',
     appId: 'com.facebook.katana',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Facebook_Icon_%28Single_Path_-_Transparent_%22f%22%29.svg/1024px-Facebook_Icon_%28Single_Path_-_Transparent_%22f%22%29.svg.png',
     developerWebsite: 'https://facebook.com/'
   },
   {
+    title: 'Google Calendar',
     appId: 'com.google.android.calendar',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Google_Calendar_icon.svg/1051px-Google_Calendar_icon.svg.png',
     developerWebsite: 'https://calendar.google.com/calendar/u/0/'
   },
   {
+    title: 'YouTube',
     appId: 'com.google.android.youtube',
+    icon: 'https://www.kanbanote.com/assets/app/images/icon_512x512.png',
     developerWebsite: 'https://www.youtube.com/'
   },
   {
-    appId: 'com.google.android.youtube',
-    developerWebsite: 'https://www.youtube.com/'
-  },
-  {
+    title: 'Gmail',
     appId: 'com.google.android.gm',
+    icon: 'https://seeklogo.com/images/G/gmail-new-2020-logo-32DBE11BB4-seeklogo.com.png',
     developerWebsite: 'https://mail.google.com/mail/u/0/#inbox'
   },
   {
+    title: 'Reddit',
     appId: 'com.reddit.frontpage',
+    icon: 'https://www.vectorico.com/download/social_media/Reddit-Icon.png',
     developerWebsite: 'http://www.reddit.com/'
   },
   {
     title: 'Hackernews',
     appId: 'com.manuelmaly.hn',
+    icon: 'https://cdn4.iconfinder.com/data/icons/socialcones/508/HackerNews-512.png',
     developerWebsite: 'https://news.ycombinator.com/'
   },
   {
+    title: 'Google',
     appId: 'com.google.android.googlequicksearchbox',
-    developerWebsite: 'https://www.google.com/'
-  },
-  {
-    appId: 'com.google.android.googlequicksearchbox',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png',
     developerWebsite: 'https://www.google.com/'
   },
   {
     title: 'Google Keep',
     appId: 'com.google.android.keep',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Google_Keep_icon.svg/1024px-Google_Keep_icon.svg.png',
     developerWebsite: 'https://keep.google.com/u/0/'
   },
   {
