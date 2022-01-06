395034fbfb60ad43f08554b8a754820d550b0096
Farbod Saraf <farbodsaraf@users.noreply.github.com>
2022-01-06 23:39:45 +0100

    Update pngs to rounded corners

--- src/data/websites.js
+++ src/data/websites.js
@@ -18,37 +18,37 @@ const websites = [
   {
     title: 'Twitter',
     appId: 'com.twitter.android',
-    icon: 'https://play-lh.googleusercontent.com/wIf3HtczQDjHzHuu7vezhqNs0zXAG85F7VmP7nhsTxO3OHegrVXlqIh_DWBYi86FTIGk',
+    icon: 'https://www.vhv.rs/dpng/d/146-1461722_twitter-circle-twitter-logo-png-transparent-png.png',
     developerWebsite: 'https://twitter.com/'
   },
   {
     title: 'Facebook',
     appId: 'com.facebook.katana',
-    icon: 'https://play-lh.googleusercontent.com/ccWDU4A7fX1R24v-vvT480ySh26AYp97g1VrIB_FIdjRcuQB2JP2WdY7h_wVVAeSpg',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png',
     developerWebsite: 'https://facebook.com/'
   },
   {
     title: 'Google Calendar',
     appId: 'com.google.android.calendar',
-    icon: 'https://play-lh.googleusercontent.com/Jsbb0EeesKUbDTl3UyDKO6sNz45RCMh7gnoI6giQcQz1f5Mj0J4TRh7Psyu53vShh-qm',
+    icon: 'https://okappyimages.okappy.com/google-calendar-icon.png',
     developerWebsite: 'https://calendar.google.com/calendar/u/0/'
   },
   {
     title: 'YouTube',
     appId: 'com.google.android.youtube',
-    icon: 'https://play-lh.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc',
+    icon: 'https://toppng.com/uploads/preview/youtube-icon-logo-transparent-youtube-11553542027nec8a4iwhh.png',
     developerWebsite: 'https://www.youtube.com/'
   },
   {
     title: 'Gmail',
     appId: 'com.google.android.gm',
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg',
+    icon: 'https://image.similarpng.com/very-thumbnail/2020/12/Gmail-logo-design-on-transparent-background-PNG.png',
     developerWebsite: 'https://mail.google.com/mail/u/0/#inbox'
   },
   {
     title: 'Reddit',
     appId: 'com.reddit.frontpage',
-    icon: 'https://www.vectorico.com/download/social_media/Reddit-Icon.png',
+    icon: 'https://toppng.com/uploads/preview/reddit-icon-reddit-logo-transparent-115628752708pqmsy4kgm.png',
     developerWebsite: 'http://www.reddit.com/'
   },
   {
@@ -66,13 +66,13 @@ const websites = [
   {
     title: 'Google Keep',
     appId: 'com.google.android.keep',
-    icon: 'https://play-lh.googleusercontent.com/zGDJ3nGGv_QqhHoY6wXI1qvG9eo-bj03HGVreITCkr-BIfVfGEvnczJKsNzgiC57Ds0',
+    icon: 'https://cdn-icons-png.flaticon.com/512/2965/2965358.png',
     developerWebsite: 'https://keep.google.com/u/0/'
   },
   {
     title: 'SoundCloud',
     appId: 'com.soundcloud.android',
-    icon: 'https://play-lh.googleusercontent.com/lvYCdrPNFU0Ar_lXln3JShoE-NaYF_V-DNlp4eLRZhUVkj00wAseSIm-60OoCKznpw',
+    icon: 'http://assets.stickpng.com/thumbs/58af04446c252499281ae91f.png',
     developerWebsite: 'https://soundcloud.com/'
   },
   {
